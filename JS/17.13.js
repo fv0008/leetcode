5ffe4c26bc80e96aac60301ddfc4e270f9aeb4fa
fanxiaolong <developer@lunarhook.com>
2020-07-09 03:20:20 +0800

    1

---
+++ JS/17.13.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {number}
+ */
+var respace = function(dictionary, sentence) {
+    var size = sentence.length
+    dictionary = dictionary.filter((i,index,self)=>{
+        return self.indexOf(i) == index
+    })
+    for(var i = dictionary.length;i>=0;i--)
+    {
+        if(-1==sentence.indexOf(dictionary[i]))
+        {
+            dictionary.splice(i,1)
+        }
+    }
+    dictionary.sort((a,b)=>{return b.length-a.length})
+    var s = 0
+    while(s<dictionary.length)
+    {
+        sentence = sentence.replace(RegExp(dictionary[s],"g")," ")
+        s++
+    }
+    sentence = sentence.replace(/\s/g,"")
+    var r = sentence.length
+    return r
+};
+
+//respace(["looked","just","like","her","brother"],"jesslookedjustliketimherbrother")
+//respace(["potimzz"],"potimzzpotimzz")
+respace(["bt","vbtbvtvvbbvtbvvbbbvbtbbv","bvvbbbvvvbvttbtbvtvtvvbttbbbtvvvb","btttbvbbbtbbtbvvttbvbvtvbtbbttb","bt","vvbvbvbvtbvbvvvvtv","tbvvvtttvtbvbtttvvbtvvvvtvvbvvtvvbbvbbbvb","v","bvb","vvtbvtvbttbttvvbvttbt","btbtbtttvbbtbttbtvvttbvtbtvtbvvtbbbb","bttbvbbttvvbtvvvvb","bvvbvbvttbvtbvvtbttvvvvtvbtvbttbbvvtvtvv","vbtttt","btbvbbbvbtvtbvvv","b","tbtbtv","vbvbbvvbvbbvvb","vbvvtvbvbvbttvbvbtvbtbtvtbvbbtb","bvvbvvttttttbtvvvttvbvtvvbvtbtvtbvttvvtbt","bvtbttv","bbbt","vtt","ttvv","b","vbb","vtvvbtttvtbbvvbbtbb","vvv","vvvvbbbtbbbvbbbb","ttvvbtvv","v","btvbbvtbbvbvtvttvvbbbtbvvvtbtb","vv","btbttbtbbvbvvbvttbttvtbbtbvtttvbbtbbtvtvvvvbbttvtvvbbbv","ttvbbbbttbtbtb","tvvbvbvvb","vv","ttbttvtvbtbbbbv","bvvvtbbvvvbtvbvtvtvvvvbb","vtbvvbvvvvvttvbbttbbvtvt","tbvbbt","b","v","tvbbtvvtvvtbtbttvvvb","tbttbttbbbtbtvtvtvtbbbvvtbbbvbbvvvbbttvvt","bbvttvtvvtbvbbttvbbtbvvttbvbvbtbvvvbbbvbvbvbtvbtvvvtvvtbttbttbbvbbbttvvvbvvtb","vttvvbvv","tbttbvvttvbtvvtbbvvv","bvbbbbbbbb","vtbvvtbbvtt","bvttbvvbvb","tvttttbbvvvbbtttvvv"],"btbvtttttbvttbvvbbtvvbvbvvbtbtbtvbtttbvbbbtbbtbvvttbvbvtvbtbbttbvvbvbtttbvttbvvbbvvv")
