9cae590e259ac49f3a37457b9aeb82de637d7846
fanxiaolong <developer@lunarhook.com>
2020-07-27 11:52:55 +0800

    Update 求阅读连续章节内容的最长时间.sql

--- SQL/求阅读连续章节内容的最长时间.sql
+++ SQL/求阅读连续章节内容的最长时间.sql
@@ -31,3 +31,16 @@ group by t1.userid order by timelong desc
 /*
 最终是阅读章节和时间的总长度，如果求章节最长，order by timelong desc 替换成 order by  chapters desc
 */
+
+set @num = 0
+select
+       min(t.charpter ) as begin,max(t.charpter ) as end,max(t.charpter )-min(t.charpter )+1 as chapters,sum(t.ltime) as timelong,grp
+from(select *,@num:=@num+1  as row1,@num  - charpter as grp from qc as q where exists(select qc.charpter from qc where charpter=q.charpter+1 and q.userid=userid) or exists(select qc.charpter from qc where charpter=q.charpter-1 and q.userid=userid)
+           ) t
+group by t.grp
+
+/*
+相对来说用@num或者row_number()方式更好，比left join和exists的方式扫描表的时间少，可能需要处理下ONLY_FULL_GROUP_BY
+set @@sql_mode ='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'
+就可以了
+*/
